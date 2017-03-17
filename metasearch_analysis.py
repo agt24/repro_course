6826c7e0052f6cd16089781a0e98eefb93bdfa6a
Adam Thomas <git@adamthomas.us>
2017-03-17 08:55:11 -0400

    Add file to repro

---
+++ metasearch_analysis.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# get_ipython().system('git clone https://github.com/OpenNeuroLab/metasearch.git')
+
+from pathlib import Path
+metasearch_dir= Path('data_not_in_repo/metasearch/')
+data_exists = metasearch_dir.exists()
+in_repro_course = Path.cwd().name == 'repro_course'
+if data_exists and in_repro_course:
+    print('data has been downloaded')
+else:
+    print('error: data is not downloaded')
+    assert(False)
